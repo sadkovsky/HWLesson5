8decf76d4a71d6743cf28c3cce3badb7275c0199
sadkovsky <ivan.volovenko@gmail.com>
2015-08-17 20:04:17 -0400

    first commit

---
+++ HW5.js
@@ -0,0 +1,14 @@
+var num1 = 2344;
+var num2 = 2324343545;
+var result = "num1" + "num2";
+
+console.log(num1+num2);
+console.log(result);
+
+var sentence1 = "The men is";
+var sentence2 = "relly fat";
+var wholeSentence = sentence1 + sentence2
+
+console.log(wholeSentence);
+
+var smalltalk ={ breed: "samoyed", are:3, isAJerk: true};
